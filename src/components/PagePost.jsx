ff991a316db0ed02bab1c97d8fc9d4b0a12f0eff
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-06-22 14:43:15 +0500

    updates

--- src/components/PagePost.jsx
+++ src/components/PagePost.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import "./PostSec.css";
+import { BsThreeDots } from "react-icons/bs";
 
 const PagePost = () => {
   return (
     <div className="pagePost">
       <div className="postHeader">
         <div className="headerLeft">
-          <img src="" alt="logo" className="pageLogo" />
+          <img
+            className="pageLogo"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQEQl3TdXRZAxQ/profile-displayphoto-shrink_100_100/0/1561463236720?e=1629936000&v=beta&t=s-fGyByCn8y7cn8DnUjlwactRwosFBXbA-4SbQrcILs"
+            alt="profile"
+          />
           <div className="pagePostInfo">
-            <p>Page Name</p>
+            <p className="mb-0" style={{ fontWeight: "600" }}>
+              Page Name
+            </p>
             <small className="follower">92,991,222 followers</small>
             <small className="status">Promoted</small>
           </div>
         </div>
-        <div className="headerRight"></div>
+        <div className="headerRight">
+          <BsThreeDots />
+        </div>
       </div>
     </div>
   );
