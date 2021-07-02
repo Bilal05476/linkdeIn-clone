9890b8c990b5ad1e26fcadaa3682e3c6bfa664e4
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-02 22:08:08 +0500

    updates

--- src/components/ViewProUserDetails.jsx
+++ src/components/ViewProUserDetails.jsx
@@ -29,15 +29,45 @@ const ViewProUserDetails = ({
           }}
         />
       )}
-      <div className="proUserDetails">
-        <h4>{userName}</h4>
-        <p className="m-0">{userOccupation}</p>
-        <small
-          className="m-0"
-          style={{ color: toggleTheme ? "rgb(139, 139, 139)" : "#ccc" }}
-        >
-          {userCity}, {userCountry}
-        </small>
+      <div className="d-flex">
+        <div className="proUserDetails">
+          <h4>{userName}</h4>
+          <p className="m-0">{userOccupation}</p>
+          <small
+            className="m-0"
+            style={{ color: toggleTheme ? "rgb(139, 139, 139)" : "#ccc" }}
+          >
+            Talks about #reactjs, #jamstack, #serverless, #mernstackdeveloper,
+            and #websitedevelopment <br />
+            {userCity}, {userCountry}
+          </small>
+          <div className="profileAnalytics">
+            <small>1,559 followers 500+ connections</small>
+          </div>
+          <div className="profileBtn">
+            <button>Open to</button>
+            <button>Add section</button>
+            <button>More</button>
+          </div>
+        </div>
+        <div className="userPageDetails">
+          <div className="userPageInfo">
+            <img
+              className=""
+              src="https://dynamic.brandcrowd.com/asset/logo/9971bc9b-9d51-4b42-beca-b3ffe882efcd/logo?v=637102751107770000"
+              alt="profile"
+            />
+            <h4 className="">Page Name</h4>
+          </div>
+          <div className="userProfession d-flex">
+            <img
+              className=""
+              src="https://dynamic.brandcrowd.com/asset/logo/9971bc9b-9d51-4b42-beca-b3ffe882efcd/logo?v=637102751107770000"
+              alt="profile"
+            />
+            <h4 className="">Company Name</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
